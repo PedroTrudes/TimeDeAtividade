397e5689b3e22c84021a75a9acc13b5a410b019d
PedroTrudes <pedro.jtrudes@gmail.com>
2022-11-03 19:51:17 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <Forms />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Forms from './components/Forms.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Forms
+  }
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Forms.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulario para criação de nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>{{tempoDecorrido}}</strong>
+                    </section>
+                    <button class="button" @click="iniciar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Forms',
+    data(){
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        tempoDecorrido() : string{
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods: {
+        iniciar() {
+            //começando com a contagem
+            // 1seg = 1000ms
+            this.cronometro = setInterval(()=>{
+                this.tempoEmSegundos ++
+                console.log('incrementando o contador');
+            }, 1000)
+            console.log("iniciando")
+
+        },
+        finalizar(){
+            console.log("Finalizando")
+            clearInterval(this.cronometro);
+        },
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
